2182fc2ea982ecf03fdc0e535786d8a95b60368d
BaptisteMarechaux <baptiste.marechaux@gmail.com>
2015-01-29 00:14:10 +0100

    28-01 Update
    Some classes were added...

---
+++ .src/Clay Game/Assets/Scripts/BattleEntity.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class BattleEntity : MonoBehaviour {
+	protected int hp;
+	protected int hpMax;
+	protected int nickname;
+
+	public virtual void ChangeHP(int amount)
+	{
+		hp+=amount;
+		if(hp<0) hp=0;
+		if(hp>hpMax) hp = hpMax;
+	}
+		
+}
---
+++ .src/Clay Game/Assets/Scripts/BattleUnit.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BattleUnit : BattleEntity {
+	/// Classe décrivant une unité, ses statistiques et certaines de ses actions
+	int power; //Puissance de l'unité
+	int resist; //Résistance de l'unité
+	int movement;
+	int range;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
