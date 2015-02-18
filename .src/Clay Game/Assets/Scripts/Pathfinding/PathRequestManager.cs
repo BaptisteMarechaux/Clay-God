8d118665294adc998037f3cc5bc4b8e3ef5fdcbe
BaptisteMarechaux <baptiste.marechaux@gmail.com>
2015-02-18 01:30:42 +0100

    aaaaaa 18-02

--- .src/Clay Game/Assets/Scripts/Pathfinding/PathRequestManager.cs
+++ .src/Clay Game/Assets/Scripts/Pathfinding/PathRequestManager.cs
@@ -4,9 +4,56 @@ using System.Collections.Generic;
 using System;
 
 public class PathRequestManager : MonoBehaviour {
+    Queue<PathRequest> pathRequestQueue = new Queue<PathRequest>();
+    PathRequest currenPathRequest;
 
+    static PathRequestManager instance;
+    PathFinding pathfinding;
+
+    bool isProcessingPath;
+
+    void Awake()
+    {
+        instance = this;
+        pathfinding = GetComponent<PathFinding>();
+    }
 	public static void RequestPath(Vector3 pathStart, Vector3 pathEnd, Action<Vector3[], bool> callback)
     {
+        PathRequest newRequest = new PathRequest(pathStart, pathEnd, callback);
+        instance.pathRequestQueue.Enqueue(newRequest);
+        instance.TryProcessNext();
 
+        
+    }
+
+    void TryProcessNext()
+    {
+        if(!isProcessingPath && pathRequestQueue.Count > 0)
+        {
+            currenPathRequest = pathRequestQueue.Dequeue();
+            isProcessingPath = true;
+            pathfinding.StartFindPath(currenPathRequest.pathStart, currenPathRequest.pathEnd);
+        }
+    }
+
+    public void FinishedProcessingPath(Vector3 path, bool success)
+    {
+        currenPathRequest.callback(path, success);
+        isProcessingPath = false;
+        TryProcessNext();
+    }
+
+    struct PathRequest
+    {
+        public Vector3 pathStart;
+        public Vector3 pathEnd;
+        public Action<Vector3[], bool> callback;
+
+        public PathRequest(Vector3 start, Vector3 end, Action<Vector3[], bool> Callback)
+        {
+            pathStart = start;
+            pathEnd = end;
+            callback = Callback;
+        }
     }
 }
