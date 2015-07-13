10ea91df4f9d3cf334531eeb20706fb3a0ef1a55
BaptisteMarechaux <baptiste.marechaux@gmail.com>
2015-07-13 02:07:17 +0200

    Options Menu

--- .src/Clay Game/Assets/Scripts/Main Menu/MainMenuManager.cs
+++ .src/Clay Game/Assets/Scripts/Main Menu/MainMenuManager.cs
@@ -46,4 +46,6 @@ public class MainMenuManager : MonoBehaviour {
     {
         Application.LoadLevel("TutorialScene");
     }
+
+   
 }
---
+++ .src/Clay Game/Assets/Scripts/Main Menu/OptionsManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class OptionsManager : MonoBehaviour {
+
+    [SerializeField]
+    private Slider volumeSlider;
+    [SerializeField]
+    private Toggle fullscreenToggle;
+
+    public GameObject mainMenuCanvas;
+    public GameObject optionsCanvas;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void VolumeChange()
+    {
+
+    }
+
+    public void ResolutionChange(int screenH)
+    {
+        Screen.SetResolution(Mathf.CeilToInt(screenH*1.7777f), screenH, fullscreenToggle.isOn);
+    }
+
+    public void QualitySettingsChange(int value)
+    {
+        switch(value)
+        {
+            case 0:
+                QualitySettings.SetQualityLevel(0,true);
+                break;
+            case 1:
+                QualitySettings.SetQualityLevel(3, true);
+                break;
+
+            case 2:
+                QualitySettings.SetQualityLevel(5, true);
+                break;
+
+            default :
+                QualitySettings.SetQualityLevel(3, true);
+                break;
+        }
+    }
+
+    public void backFromOptions()
+    {
+        mainMenuCanvas.SetActive(true);
+        optionsCanvas.SetActive(false);
+    }
+
+     public void ShowOptions()
+    {
+        optionsCanvas.SetActive(true);
+        mainMenuCanvas.SetActive(false);
+    }
+}
